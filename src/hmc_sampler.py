6a0dda989c4f8f9c168204c68741bdb57b854d81
vrettasm <vrettasm@gmail.com>
2022-06-30 14:37:16 +0200

    Updated grad-check output with scientific notation ":.3E".

--- src/hmc_sampler.py
+++ src/hmc_sampler.py
@@ -537,7 +537,7 @@ class HMC(object):
 
             # Display the error information.
             print(f"Chain -> {chain}: "
-                  f"Grad-Check error at t=tf: {diff_error}", flush=True)
+                  f"Grad-Check error at t=tf: {diff_error:.3E}", flush=True)
         # _end_if_
 
         # Return the local dictionary.
@@ -573,7 +573,7 @@ class HMC(object):
             diff_error = check_grad(_func, _grad, x.copy(), *args)
 
             # Display the error.
-            print(f"Grad-Check error at t=0: {diff_error}", end='\n\n')
+            print(f"Grad-Check error at t=0: {diff_error:.3E}\n")
 
         # _end_if_
 
